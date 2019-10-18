b4fde9192e5f53cae9f339e7002039beb24a21a5
Muhammad Bimo <47705538+muhammadbimo1@users.noreply.github.com>
2019-10-19 02:09:33 +0800

    Initial commit

---
+++ app/src/main/java/com/a243777/busschedule/Weekdayschedule.java
@@ -0,0 +1,13 @@
+package com.a243777.busschedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Weekdayschedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekdayschedule);
+    }
+}
